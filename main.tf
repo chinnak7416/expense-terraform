c7bb3a55417acd5b9fe3d01407ff2dba953f9597
RAMBABU <chinnak7416@gmail.com>
2024-07-11 20:18:31 +0530

    commit for backend mysql

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "frontend" {
-  depends_on    = [module.backend]
+#  depends_on    = [module.backend]
 
   source        = "./modules/app"
   instance_type = var.instance_type
@@ -9,23 +9,23 @@ module "frontend" {
   vault_token   = var.vault_token
 }
 
-module "backend" {
-  depends_on    = [module.mysql]
-
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "backend"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
-}
-
-module "mysql" {
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "mysql"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
-
-}
+#module "backend" {
+#  depends_on    = [module.mysql]
+#
+#  source        = "./modules/app"
+#  instance_type = var.instance_type
+#  component     = "backend"
+#  env           = var.env
+#  zone_id       = var.zone_id
+#  vault_token   = var.vault_token
+#}
+#
+#module "mysql" {
+#  source        = "./modules/app"
+#  instance_type = var.instance_type
+#  component     = "mysql"
+#  env           = var.env
+#  zone_id       = var.zone_id
+#  vault_token   = var.vault_token
+#
+#}
