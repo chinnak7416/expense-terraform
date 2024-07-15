5001b028b87357ecb517e840ed3ece1a9eb450b2
RAMBABU <chinnak7416@gmail.com>
2024-07-15 20:24:11 +0530

    created all component vpc peering methods

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,7 +1,9 @@
 variable "env" {}
 variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr_id" {}
 variable "default_route_table_id" {}
-
+variable "frontend_subnets" {}
+variable "backend_subnets" {}
+variable "db_subnets" {}
+variable "availability_zones" {}
