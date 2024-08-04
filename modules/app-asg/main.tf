59c9800d459707c48e4f797a050749f9db4a80c8
RAMBABU <chinnak7416@gmail.com>
2024-08-04 11:44:32 +0530

    Update Prometheus monitor tag to Modules in asg

--- modules/app-asg/main.tf
+++ modules/app-asg/main.tf
@@ -66,6 +66,11 @@ resource "aws_autoscaling_group" "main" {
     propagate_at_launch = true
     value               = "${var.component}-${var.env}"
   }
+  tag {
+    key                 = "monitor"
+    propagate_at_launch = true
+    value               = "${var.component}-${var.env}"
+  }
 }
 
 resource "aws_lb_target_group" "main" {
