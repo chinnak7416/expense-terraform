7fa8aebc28b2d8ff9bb12443acbd760bf1b24c41
RAMBABU <chinnak7416@gmail.com>
2024-08-04 11:28:57 +0530

    Add bool to dependencies

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-pull -i localhost, -U https://github.com/chinnak7416/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component} -e vault_token=${vault_token} &>>/opt/ansible.log
-ansible-pull -i localhost, -U https://github.com/chinnak7416/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json only_deployment: false &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/chinnak7416/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json -e only_deployment: false &>>/opt/ansible.log
