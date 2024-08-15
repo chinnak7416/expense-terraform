39b58c8a7392a6dda880364ee166f3a57afd8a4f
RAMBABU <chinnak7416@gmail.com>
2024-08-15 23:05:01 +0530

    Creating eks cluster and nodes

--- main.tf
+++ main.tf
@@ -21,73 +21,73 @@
 #  lb_ports                = {http: 80, https: 443}
 #  kms_key_id              = var.kms_key_id
 #}
-module "frontend" {
-  depends_on               = [module.backend]
-  source                   = "./modules/app-asg"
-
-  app_port                 = 80
-  bastion_nodes            = var.bastion_nodes
-  component                = "frontend"
-  env                      = var.env
-  instance_type            = var.instance_type
-  max_capacity             = var.max_capacity
-  min_capacity             = var.min_capacity
-  prometheus_nodes         = var.prometheus_nodes
-  server_app_port_sg_cidr  = var.public_subnets
-  subnets                  = module.vpc.frontend_subnets
-  vault_token              = var.vault_token
-  vpc_id                   = module.vpc.vpc_id
-  certificate_arn          = var.certificate_arn
-  lb_app_port_sg_cidr      = ["0.0.0.0/0"]
-  lb_ports                 = { http: 80, https: 443 }
-  lb_subnets               = module.vpc.public_subnets
-  lb_type                  = "public"
-  zone_id                  = var.zone_id
-  kms_key_id              = var.kms_key_id
-}
-
-module "backend" {
-  depends_on               = [module.rds]
-  source                   = "./modules/app-asg"
-
-  app_port                 = 8080
-  bastion_nodes            = var.bastion_nodes
-  component                = "backend"
-  env                      = var.env
-  instance_type            = var.instance_type
-  max_capacity             = var.max_capacity
-  min_capacity             = var.min_capacity
-  prometheus_nodes         = var.prometheus_nodes
-  server_app_port_sg_cidr  = concat(var.frontend_subnets, var.backend_subnets)
-  subnets                  = module.vpc.backend_subnets
-  vault_token              = var.vault_token
-  vpc_id                   = module.vpc.vpc_id
-  certificate_arn          = var.certificate_arn
-  lb_app_port_sg_cidr      = var.frontend_subnets
-  lb_ports                 = { http: 8080 }
-  lb_subnets               = module.vpc.backend_subnets
-  lb_type                  = "private"
-  zone_id                  = var.zone_id
-  kms_key_id              = var.kms_key_id
-}
-
-module "rds" {
-  source                  = "./modules/rds"
-
-  allocated_storage       = 20
-  component               = "rds"
-  engine                  = "mysql"
-  engine_version          = "8.0.36"
-  env                     = var.env
-  family                  = "mysql8.0"
-  instance_class          = "db.t3.micro"
-  skip_final_snapshot     = true
-  storage_type            = "gp3"
-  subnet_ids              = module.vpc.db_subnets
-  vpc_id                  = module.vpc.vpc_id
-  server_app_port_sg_cidr = var.backend_subnets
-  kms_key_id              = var.kms_key_id
-}
+#module "frontend" {
+#  depends_on               = [module.backend]
+#  source                   = "./modules/app-asg"
+#
+#  app_port                 = 80
+#  bastion_nodes            = var.bastion_nodes
+#  component                = "frontend"
+#  env                      = var.env
+#  instance_type            = var.instance_type
+#  max_capacity             = var.max_capacity
+#  min_capacity             = var.min_capacity
+#  prometheus_nodes         = var.prometheus_nodes
+#  server_app_port_sg_cidr  = var.public_subnets
+#  subnets                  = module.vpc.frontend_subnets
+#  vault_token              = var.vault_token
+#  vpc_id                   = module.vpc.vpc_id
+#  certificate_arn          = var.certificate_arn
+#  lb_app_port_sg_cidr      = ["0.0.0.0/0"]
+#  lb_ports                 = { http: 80, https: 443 }
+#  lb_subnets               = module.vpc.public_subnets
+#  lb_type                  = "public"
+#  zone_id                  = var.zone_id
+#  kms_key_id              = var.kms_key_id
+#}
+#
+#module "backend" {
+#  depends_on               = [module.rds]
+#  source                   = "./modules/app-asg"
+#
+#  app_port                 = 8080
+#  bastion_nodes            = var.bastion_nodes
+#  component                = "backend"
+#  env                      = var.env
+#  instance_type            = var.instance_type
+#  max_capacity             = var.max_capacity
+#  min_capacity             = var.min_capacity
+#  prometheus_nodes         = var.prometheus_nodes
+#  server_app_port_sg_cidr  = concat(var.frontend_subnets, var.backend_subnets)
+#  subnets                  = module.vpc.backend_subnets
+#  vault_token              = var.vault_token
+#  vpc_id                   = module.vpc.vpc_id
+#  certificate_arn          = var.certificate_arn
+#  lb_app_port_sg_cidr      = var.frontend_subnets
+#  lb_ports                 = { http: 8080 }
+#  lb_subnets               = module.vpc.backend_subnets
+#  lb_type                  = "private"
+#  zone_id                  = var.zone_id
+#  kms_key_id              = var.kms_key_id
+#}
+#
+#module "rds" {
+#  source                  = "./modules/rds"
+#
+#  allocated_storage       = 20
+#  component               = "rds"
+#  engine                  = "mysql"
+#  engine_version          = "8.0.36"
+#  env                     = var.env
+#  family                  = "mysql8.0"
+#  instance_class          = "db.t3.micro"
+#  skip_final_snapshot     = true
+#  storage_type            = "gp3"
+#  subnet_ids              = module.vpc.db_subnets
+#  vpc_id                  = module.vpc.vpc_id
+#  server_app_port_sg_cidr = var.backend_subnets
+#  kms_key_id              = var.kms_key_id
+#}
 
 
 module "vpc" {
